3cb35ea7c3aba3e4aabf3e2f75633c3339fa5621
XuanAnh263 <117911834+XuanAnh263@users.noreply.github.com>
2022-12-15 14:22:58 +0700

    Update Movie.java

--- java/Encapsulation_Inheritance/src/home_work_day08/model/Movie.java
+++ java/Encapsulation_Inheritance/src/home_work_day08/model/Movie.java
@@ -10,6 +10,15 @@ public class Movie extends Film{
         super(id, title, category, director, premiereDate);
         this.time = time;
     }
+    
+    public void display() {
+        System.out.println("ID " + this.getId());
+        System.out.println("TITLE " + this.getTitle());
+        System.out.println("CATEGORY " + this.getCategory());
+        System.out.println("DIRECTOR " + this.getDirector());
+        System.out.println("PREMIEREDATE " + this.getPremiereDate());
+        System.out.println("TIME " + this.getTime());
+    }
 
     public int getTime() {
         return time;
