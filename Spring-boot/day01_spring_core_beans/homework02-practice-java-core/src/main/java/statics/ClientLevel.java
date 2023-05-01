a29e5126a3b3a36a19a3ee17b26971981bac5c10
XuanAnh263 <117911834+XuanAnh263@users.noreply.github.com>
2023-05-01 13:46:48 +0700

    Update ClientLevel.java

--- Spring-boot/day01_spring_core_beans/homework02-practice-java-core/src/main/java/statics/ClientLevel.java
+++ Spring-boot/day01_spring_core_beans/homework02-practice-java-core/src/main/java/statics/ClientLevel.java
@@ -1,17 +1,9 @@
 package statics;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@NoArgsConstructor
-@AllArgsConstructor
 public enum ClientLevel {
-    LEVEL_1(1),
-    LEVEL_2(2),
-    LEVEL_3(3);
-
-
-    public int level;
+    ca_nhan,
+    dai_dien_don_vi_kinh_doanh,
+    dai_dien_don_vi_hanh_chinh;
+    
 }
